65e4b6cdbb51fd8f17f9570e33e5fa64ca3f3140
Felix Seidl <30559812+felixse@users.noreply.github.com>
2020-01-24 11:20:14 +0100

    list available languages (#15)
    * list available languages
    
    * fix tests
    
    * fix review findings

--- README.md
+++ README.md
@@ -22,6 +22,7 @@ import { CalingaBackend, CalingaBackendOptions } from 'i18next-calinga-backend';
 ...
 
 const backendOptions: CalingaBackendOptions = {
+    project: '<YOUR_PROJECT_NAME_HERE>'
     version: 'v1',
     resources: {
         en: {
@@ -46,8 +47,10 @@ For use in React or React Native also add the following lines to the init option
 
 ```ts
 react: {
-  bindI18n: 'loaded'
+    bindI18n: 'loaded';
 }
 ```
+
+Available languages can be accessed at `CalingaBackend.languages` or by addding a handler for `CalingaBackend.onLanguageChanged`.
 
 For a full integration sample for react native including a cache have a look [here](https://github.com/conplementAG/calinga-react-native-demo).
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,150 +1,198 @@
-import i18next from "i18next";
-import { CalingaBackend, CalingaBackendOptions } from "./";
-import axios from "axios";
-import { mocked } from 'ts-jest/utils'
+import i18next from 'i18next';
+import { CalingaBackend, CalingaBackendOptions } from './';
+import axios from 'axios';
+import { mocked } from 'ts-jest/utils';
 
-const keyName = "origin";
-const language = "en";
-const namespace = "default";
+const keyName = 'origin';
+const language = 'en';
+const namespace = 'default';
 
-const fromResourcesTranslation = "from resources";
-const fromCacheTranslation = "from cache";
-const fromServiceTranslation = "from service";
+const fromResourcesTranslation = 'from resources';
+const fromCacheTranslation = 'from cache';
+const fromServiceTranslation = 'from service';
 
-jest.mock("axios");
+jest.mock('axios');
 const axiosMock = mocked(axios, true);
 i18next.init();
 let options: CalingaBackendOptions;
 
-describe("read", () => {
-  beforeEach(() => {
-    options = {};
-  })
+describe('read', () => {
+    beforeEach(() => {
+        options = {
+            project: 'example',
+            serviceBaseUrl: 'https://api.calinga.io/v1/'
+        };
+    });
 
-  describe("service not reachable", () => {
-    describe("no cache configured", () => {
-      describe("no resources provided", () => {
-        it("should return nothing", done => {
-          setupService(false);
-          const backend = new CalingaBackend(i18next.services, options);
+    describe('service not reachable', () => {
+        describe('no cache configured', () => {
+            describe('no resources provided', () => {
+                it('should return nothing', done => {
+                    setupServiceUnavailable();
+                    const backend = new CalingaBackend(i18next.services, options, {});
 
-          backend.read(language, namespace, (error, data) => {
-            expect(data).toBeUndefined();
-            done();
-          });
-        })
-      })
+                    backend.read(language, namespace, (error, data) => {
+                        expect(data).toBeUndefined();
+                        done();
+                    });
+                });
+            });
 
-      describe("resources provided", () => {
-        it("should return translations from resources", done => {
-          setupService(false);
-          setupResources();
-          const backend = new CalingaBackend(i18next.services, options);
+            describe('resources provided', () => {
+                it('should return translations from resources', done => {
+                    setupServiceUnavailable();
+                    setupResources();
+                    const backend = new CalingaBackend(i18next.services, options, {});
 
-          backend.read(language, namespace, (error, data) => {
-            expect(data).toBeDefined();
-            expect(data[keyName]).toBe(fromResourcesTranslation);
-            done();
-          });
-        })
-      })
-    })
-
-    describe("cache configured", () => {
-      it("should return translations from cache", done => {
-        setupService(false);
-        setupResources();
-        setupCache();
-        const backend = new CalingaBackend(i18next.services, options);
-
-        backend.read(language, namespace, (error, data) => {
-          expect(data).toBeDefined();
-          expect(data[keyName]).toBe(fromCacheTranslation);
-          done();
+                    backend.read(language, namespace, (error, data) => {
+                        expect(data).toBeDefined();
+                        expect(data[keyName]).toBe(fromResourcesTranslation);
+                        done();
+                    });
+                });
+            });
         });
-      })
-    })
-  })
 
-  describe("service reachable", () => {
-    console.log("broken tset");
-    it("should return translations from service", done => {
-      setupService(true);
-      setupCache();
-      setupResources();
+        describe('cache configured', () => {
+            it('should return translations from cache', done => {
+                setupServiceUnavailable();
+                setupResources();
+                setupCache();
+                const backend = new CalingaBackend(i18next.services, options, {});
 
-      const backendConnectorMock = {
-        loaded: (name, err, data) => {
-          expect(data).toMatchObject({ [keyName]: fromServiceTranslation});
-          done();
-        }
-      }
-
-      const backend = new CalingaBackend({ ...i18next.services, backendConnector: backendConnectorMock}, options);
-
-      backend.read(language, namespace, (error, data) => {
-        expect(data).toBeDefined();
-        expect(data[keyName]).toBe(fromCacheTranslation);
-      });
-    })
-
-    describe("cache configured", () => {
-      it("writes response to cache", done => {
-        setupService(true);
-        setupCache();
-        setupResources();
-
-        const backendConnectorMock = {
-          loaded: (name, err, data) => {
-            expect(data).toMatchObject({ [keyName]: fromServiceTranslation});
-            done();
-          }
-        }
-
-        const backend = new CalingaBackend({ ...i18next.services, backendConnector: backendConnectorMock}, options);
-
-        backend.read(language, namespace, async (error, data) => {
-          const cachedData = await options.cache.read("calinga_translations_default_en")
-          expect(JSON.parse(cachedData)[keyName]).toBe(fromCacheTranslation);
+                backend.read(language, namespace, (error, data) => {
+                    expect(data).toBeDefined();
+                    expect(data[keyName]).toBe(fromCacheTranslation);
+                    done();
+                });
+            });
         });
-      })
-    })
-  })
+    });
+
+    describe('service reachable', () => {
+        it('should return translations from service', done => {
+            setupServiceAvailable();
+            setupCache();
+            setupResources();
+
+            const backendConnectorMock = {
+                loaded: (name, err, data) => {
+                    expect(data).toMatchObject({ [keyName]: fromServiceTranslation });
+                    done();
+                }
+            };
+
+            const backend = new CalingaBackend(
+                { ...i18next.services, backendConnector: backendConnectorMock },
+                options,
+                {}
+            );
+
+            backend.read(language, namespace, (error, data) => {
+                expect(data).toBeDefined();
+                expect(data[keyName]).toBe(fromCacheTranslation);
+                done();
+            });
+        });
+
+        describe('cache configured', () => {
+            it('writes response to cache', done => {
+                setupServiceAvailable();
+                setupCache();
+                setupResources();
+                const backendConnectorMock = {
+                    loaded: (name, err, data) => {
+                        expect(data).toMatchObject({ [keyName]: fromServiceTranslation });
+                        done();
+                    }
+                };
+
+                const backend = new CalingaBackend(
+                    { ...i18next.services, backendConnector: backendConnectorMock },
+                    options,
+                    {}
+                );
+
+                backend.read(language, namespace, async (error, data) => {
+                    const cachedData = await options.cache.read('calinga_translations_default_en');
+                    expect(JSON.parse(cachedData)[keyName]).toBe(fromCacheTranslation);
+                    done();
+                });
+            });
+        });
+    });
+});
+
+describe('init', () => {
+    describe('devmode enabled', () => {
+        it('adds cimode to the languages', done => {
+            setupServiceAvailable();
+            CalingaBackend.onLanguagesChanged = l => {
+                expect(CalingaBackend.languages).toContain('cimode');
+                expect(CalingaBackend.languages).toContain('en');
+                done();
+            };
+            const backend = new CalingaBackend(i18next.services, { ...options, devMode: true }, {});
+        });
+    });
+
+    describe('devmode not enabled', () => {
+        it('returns only languages from service', done => {
+            setupServiceAvailable();
+            CalingaBackend.onLanguagesChanged = l => {
+                expect(CalingaBackend.languages).toContain('de');
+                expect(CalingaBackend.languages).toContain('en');
+                done();
+            };
+            const backend = new CalingaBackend(i18next.services, options, {});
+        });
+    });
 });
 
 function setupResources() {
-  options.resources = {
-    [language]: {
-      [namespace]: {
-        [keyName]: fromResourcesTranslation
-      }
-    }
-  };
+    options.resources = {
+        [language]: {
+            [namespace]: {
+                [keyName]: fromResourcesTranslation
+            }
+        }
+    };
 }
 
 function setupCache() {
-  const locale = {
-    [keyName]: fromCacheTranslation
-  };
-  const cache = {
-    calinga_translations_default_en: JSON.stringify(locale)
-  };
-  options.cache = {
-    read(key: string) {
-      return Promise.resolve(cache[key]);
-    },
-    write(key: string, value: string) {
-      cache[key] = value;
-      return Promise.resolve();
-    }
-  }
+    const locale = {
+        [keyName]: fromCacheTranslation
+    };
+    const cache = {
+        calinga_translations_default_en: JSON.stringify(locale)
+    };
+    options.cache = {
+        read(key: string) {
+            return Promise.resolve(cache[key]);
+        },
+        write(key: string, value: string) {
+            cache[key] = value;
+            return Promise.resolve();
+        }
+    };
 }
 
-function setupService(available: boolean) {
-  if (available) {
-    console.log("200");
-    axiosMock.get.mockReturnValue(Promise.resolve({ status: 200, data: {[keyName]: fromServiceTranslation }}));
-  } else {
+function setupServiceUnavailable() {
     axiosMock.get.mockReturnValue(Promise.resolve({ status: 404 }));
-  }
+}
+
+function setupServiceAvailable() {
+    axiosMock.get.mockImplementation((url, o) => {
+        if (url.startsWith(options.serviceBaseUrl + 'translations')) {
+            return Promise.resolve({ status: 200, data: { [keyName]: fromServiceTranslation } });
+        } else if (url.startsWith(options.serviceBaseUrl + 'locales')) {
+            return Promise.resolve({
+                status: 200,
+                data: [
+                    { name: 'de', isReference: false },
+                    { name: 'en', isReference: true }
+                ]
+            });
+        }
+    });
 }
